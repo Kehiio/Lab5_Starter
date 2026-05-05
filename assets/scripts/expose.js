13de4054da9d89a16a255289244d9fa54d47cb93
Kelly Dempster <kellymdempster@gmail.com>
2026-05-05 13:34:35 -0700

    Add descriptive comments to js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,70 +3,51 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
+
+  /* Get interative items from html file ---- */
   const linkToExplore = document.querySelector('a[href="explore.html"]');
-  console.log(linkToExplore);
-
   const hornImg = document.querySelector('img[src="assets/images/no-image.png"]');
-  console.log(hornImg);
-
   const selectorList = document.getElementById('horn-select');
-  console.log(selectorList);
-
   const volumeSliderWrapper = document.getElementById('volume-controls');
-  console.log(volumeSliderWrapper);
-
   const volumeSlider = volumeSliderWrapper.querySelector('input[type="range"]');
   const volumeImg = volumeSliderWrapper.querySelector('img[src="assets/icons/volume-level-2.svg"]');
-
   // list of buttons (size of 1..?)
   const soundButton = document.querySelectorAll('button');
-  console.log(soundButton);
-
   const audioFile = document.getElementsByClassName('hidden')[0];
  
 
+  /* Listener functions --- */
 
-  linkToExplore.addEventListener('click', function() { 
-    // take to other page
-    console.log("Hyperlink clicked");
-
-  });
-
+  // changes img and sound based on selected horn from dropdown
   selectorList.addEventListener('change', function(event){
-    // play corresponding sound and image
+    
     let s = event.target.value;
     hornImg.src = `assets/images/${s}.svg`;
     audioFile.src = `assets/audio/${s}.mp3`;
 
-    
     audioFile.load();
-    
-
-    console.log(`changed to assets/images/${s}.svg`);
-
-
-
   });
 
+  // Changes volume level picture and alt text based on slider value 
   volumeSlider.addEventListener('input', function(){
     audioFile.volume = (volumeSlider.value / 100);
     
-    if (audioFile.volume == 0){
+    if (audioFile.volume == 0){// lvl 0, mute, 0
       
       volumeImg.src = 'assets/icons/volume-level-0.svg';
       volumeImg.alt = 'Volume level 0';
     }
-    else if (audioFile.volume < 0.33){ // lvl 1 svg
+    else if (audioFile.volume < 0.33){ // lvl 1 < 33
       
       volumeImg.src = 'assets/icons/volume-level-1.svg';
       volumeImg.alt = 'Volume level 1';
     }
-    else if (audioFile.volume < 0.67){ // lvl 2
+    else if (audioFile.volume < 0.67){ // lvl 2 < 67
       
       volumeImg.src = 'assets/icons/volume-level-2.svg';
       volumeImg.alt = 'Volume level 2';
     } 
-    else{ // lvl 3
+    else{ // lvl 3, >= 67
       
       volumeImg.src = 'assets/icons/volume-level-3.svg';
       volumeImg.alt = 'Volume level 3';
@@ -75,13 +56,12 @@ function init() {
 
   });
 
-  // for sound button
+  // Play horn on click, check for party horn
   soundButton.forEach(item => {
     item.addEventListener('click', function(event){
       
       audioFile.play();
-      console.log(`Clicked on: ${event.target.textContent}`);
-      console.log(`Volume: ${audioFile.volume}`);
+      
       if (selectorList.value == 'party-horn'){
         jsConfetti.addConfetti();
       }
@@ -89,10 +69,13 @@ function init() {
     });
   })
 
-  // set audio to match initial state of slider
+
+  /* Initialization stuff --- */
+
+  // set initial audio loudness to match initial state of slider
   audioFile.volume = 0.5;
 
-  // create confetti
+  // create confetti object
   const jsConfetti = new JSConfetti();
     
 
